fe77d2423b41a39a936ad0b065f6032aa170edc3
Leo Wang <leowll.kit@gmail.com>
2018-06-13 14:50:44 +0900

    add printArray method

--- JavaPractice/src/sort/QuickSort.java
+++ JavaPractice/src/sort/QuickSort.java
@@ -58,17 +58,23 @@ public class QuickSort {
 	public static int[] quickSort(int array[], int left, int right) {
 		if (left < right) {
 			int pivot = partition(array, left, right);
+			printArray(array);
 			quickSort(array, left, pivot - 1);
 			quickSort(array, pivot + 1, right);
 		}
 		return array;
 	}
 
-	public static void main(String[] args) {
-		int[] old_array = { 11, 3, 20, 1, 8 };
-		int[] new_array = quickSort(old_array, 0, old_array.length-1);
-		for (int i : new_array) {
+	public static void printArray(int[] arr) {
+		for (int i : arr) {
 			System.out.print(i + " ");
 		}
+		System.out.println("\n");
+	}
+
+	public static void main(String[] args) {
+		int[] old_array = { 11, 3, 20, 1, 8 };
+		int[] new_array = quickSort(old_array, 0, old_array.length - 1);
+		printArray(new_array);
 	}
 }
